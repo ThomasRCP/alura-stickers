111cf7b157d1253bce1ca0e76cef2613ffcd8126
Thomas Porciuncula <thomas.desenvolvimento@gmail.com>
2022-07-21 22:29:32 -0300

    inclusão e geração do sticker

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -29,10 +31,18 @@ public class App {
         System.out.println(listaDeFilmes.get(0));
 
         // exibir e manipular os dados
+        var geradora = new stickerGenerator();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
         }
     }
